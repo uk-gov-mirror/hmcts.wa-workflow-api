c3d628bb46d24233871ad74adfdfb8a687aba806
chrisgrimble <chris.grimble@hmcts.net>
2020-08-07 09:56:06 +0100

    Add a smoke test

---
+++ src/smokeTest/java/uk/gov/hmcts/reform/DemoSmokeTest.java
@@ -0,0 +1,12 @@
+package uk.gov.hmcts.reform;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DemoSmokeTest {
+    @Test
+    public void exampleOfTest() {
+        assertTrue(System.currentTimeMillis() > 0, "Example of Unit Test");
+    }
+}
