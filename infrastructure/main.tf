b7ae7f86c490f2f0fdad7b1ddbd3fafef0293f91
chrisgrimble <chris.grimble@hmcts.net>
2020-08-07 16:42:50 +0100

    Teraform secrets

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,21 @@
 provider "azurerm" {
   features {}
 }
+
+locals {
+  preview_vault_name     = "${var.raw_product}-aat"
+  non_preview_vault_name = "${var.raw_product}-${var.env}"
+  key_vault_name         = "${var.env == "preview" || var.env == "spreview" ? local.preview_vault_name : local.non_preview_vault_name}"
+
+}
+
+
+data "azurerm_key_vault" "wa_key_vault" {
+  name                = "${local.key_vault_name}"
+  resource_group_name = "${local.key_vault_name}"
+}
+
+data "azurerm_key_vault_secret" "testsecret" {
+  name      = "testsecret"
+  vault_uri = "${data.azurerm_key_vault.wa_key_vault.vault_uri}"
+}
