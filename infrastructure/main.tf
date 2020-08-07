0abd7a46e79fb444655567b4ef1ca5115bf5b833
chrisgrimble <chris.grimble@hmcts.net>
2020-08-07 17:01:16 +0100

    Setup secrets

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,19 +3,10 @@ provider "azurerm" {
 }
 
 locals {
-  preview_vault_name     = "${var.raw_product}-aat"
-  non_preview_vault_name = "${var.raw_product}-${var.env}"
-  key_vault_name         = "${var.env == "preview" || var.env == "spreview" ? local.preview_vault_name : local.non_preview_vault_name}"
-
+  key_vault_name                  = "${var.product}-${var.env}"
 }
-
 
 data "azurerm_key_vault" "wa_key_vault" {
   name                = "${local.key_vault_name}"
   resource_group_name = "${local.key_vault_name}"
-}
-
-data "azurerm_key_vault_secret" "testsecret" {
-  name      = "testsecret"
-  vault_uri = "${data.azurerm_key_vault.wa_key_vault.vault_uri}"
 }
