4d0af9be53f12b0fc97a4f13802a6377c57f8210
chrisgrimble <chris.grimble@hmcts.net>
2020-08-10 10:19:43 +0100

    Tidy up secrets

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,12 +1,3 @@
 provider "azurerm" {
   features {}
 }
-
-locals {
-  key_vault_name                  = "${var.product}-${var.env}"
-}
-
-data "azurerm_key_vault" "wa_key_vault" {
-  name                = "${local.key_vault_name}"
-  resource_group_name = "${local.key_vault_name}"
-}
