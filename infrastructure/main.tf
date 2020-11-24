d430627c284cc9861f9717dfe70ddf4cd87a68d6
Alejandro Baeza <42570659+alexbaeza@users.noreply.github.com>
2020-11-24 13:19:17 +0000

    Fix terraform to pull s2s secret from s2s vault and copy into wa-vault (#91)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,10 +3,6 @@ provider "azurerm" {
   features {}
 }
 
-locals {
-  S2S_SECRET_WORKFLOW_API = "${data.azurerm_key_vault_secret.s2s_secret.value}"
-}
-
 data "azurerm_key_vault" "wa_key_vault" {
   name                = "${var.product}-${var.env}"
   resource_group_name = "${var.product}-${var.env}"
@@ -21,3 +17,10 @@ data "azurerm_key_vault_secret" "s2s_secret" {
   key_vault_id = data.azurerm_key_vault.s2s_key_vault.id
   name         = "microservicekey-wa-workflow-api"
 }
+
+resource "azurerm_key_vault_secret" "s2s_secret_workflow_api" {
+  name         = "s2s-secret-workflow-api"
+  value        = data.azurerm_key_vault_secret.s2s_secret.value
+  key_vault_id = data.azurerm_key_vault.wa_key_vault.id
+}
+
