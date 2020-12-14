741396ea9c2072fea3cf513e4074c2cef1865bb6
Vamshidhar Chitti <indvamshi@gmail.com>
2020-12-14 10:47:47 +0000

    Rwa 219 (#102)
    * Added DB Tasks cleanup after each test
    
    * updated readme
    
    * fixed PR comments
    
    * fixed PR comments
    
    * Validating Task State after cleanup
    
    * lowercased jurisdiction and casetype to match with new bmpns
    
    * fixed pr comments

--- src/testUtils/java/uk/gov/hmcts/reform/waworkflowapi/SpringBootFunctionalBaseTest.java
+++ src/testUtils/java/uk/gov/hmcts/reform/waworkflowapi/SpringBootFunctionalBaseTest.java
@@ -6,11 +6,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import static net.serenitybdd.rest.SerenityRest.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @RunWith(SpringIntegrationSerenityRunner.class)
 @SpringBootTest
 @ActiveProfiles("functional")
 public abstract class SpringBootFunctionalBaseTest {
 
+    public static final String SERVICE_AUTHORIZATION = "ServiceAuthorization";
     public static final String WA_TASK_INITIATION_IA_ASYLUM = "wa-task-initiation-ia-asylum";
 
     @Value("${targets.instance}")
@@ -19,4 +24,23 @@ public abstract class SpringBootFunctionalBaseTest {
     @Value("${camunda.url}")
     public String camundaUrl;
 
+    public void cleanUp(String taskId, String token) {
+        given()
+            .header(SERVICE_AUTHORIZATION, token)
+            .contentType(APPLICATION_JSON_VALUE)
+            .baseUri(camundaUrl)
+            .basePath("/task/" + taskId + "/complete")
+            .when()
+            .post();
+
+        given()
+            .header(SERVICE_AUTHORIZATION, token)
+            .contentType(APPLICATION_JSON_VALUE)
+            .accept(APPLICATION_JSON_VALUE)
+            .baseUri(camundaUrl)
+            .when()
+            .get("/history/task?taskId=" + taskId)
+            .then()
+            .body("[0].deleteReason", is("completed"));
+    }
 }
