4124df810175907c1072236c93a9574afa7836ed
lewis150193 <lewis.williams@hmcts.net>
2021-02-02 18:52:02 +0000

    Added external task worker

--- src/main/java/uk/gov/hmcts/reform/waworkflowapi/Application.java
+++ src/main/java/uk/gov/hmcts/reform/waworkflowapi/Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableCircuitBreaker
