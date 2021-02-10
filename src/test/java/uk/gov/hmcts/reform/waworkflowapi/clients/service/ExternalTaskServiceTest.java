c9dc562911d479d06c6f73935c6abd6560d422bb
lewis150193 <lewis.williams@hmcts.net>
2021-02-10 18:04:17 +0000

    Warn task updated rebased

--- src/test/java/uk/gov/hmcts/reform/waworkflowapi/clients/service/ExternalTaskServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/waworkflowapi/clients/service/ExternalTaskServiceTest.java
@@ -5,8 +5,6 @@ import org.camunda.bpm.client.task.ExternalTaskService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 
