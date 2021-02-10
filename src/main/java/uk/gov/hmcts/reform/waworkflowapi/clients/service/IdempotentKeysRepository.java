30fcc128561e12908ebdf36bad1737d4dc498596
david.crespo <david.crespo@hmcts.net>
2021-02-10 10:44:06 +0000

    add lock

--- src/main/java/uk/gov/hmcts/reform/waworkflowapi/clients/service/IdempotentKeysRepository.java
+++ src/main/java/uk/gov/hmcts/reform/waworkflowapi/clients/service/IdempotentKeysRepository.java
@@ -1,11 +1,18 @@
 package uk.gov.hmcts.reform.waworkflowapi.clients.service;
 
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.reform.waworkflowapi.clients.model.IdempotentId;
 import uk.gov.hmcts.reform.waworkflowapi.clients.model.IdempotentKeys;
 
+import java.util.Optional;
+import javax.persistence.LockModeType;
+
 @Repository
 public interface IdempotentKeysRepository extends CrudRepository<IdempotentKeys, IdempotentId> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    public Optional<IdempotentKeys> findById(IdempotentId idempotentId);
 
 }
