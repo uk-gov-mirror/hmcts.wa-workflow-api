a5c1b1c226b279285b465b54e0390f839defde99
lewis150193 <lewis.williams@hmcts.net>
2021-02-12 12:01:51 +0000

    added functional test

--- src/functionalTest/java/uk/gov/hmcts/reform/waworkflowapi/controllers/SendMessageTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/waworkflowapi/controllers/SendMessageTest.java
@@ -7,16 +7,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import uk.gov.hmcts.reform.waworkflowapi.SpringBootFunctionalBaseTest;
 import uk.gov.hmcts.reform.waworkflowapi.clients.model.DmnValue;
 import uk.gov.hmcts.reform.waworkflowapi.clients.model.SendMessageRequest;
+import uk.gov.hmcts.reform.waworkflowapi.clients.model.idempotentkey.IdempotentId;
+import uk.gov.hmcts.reform.waworkflowapi.clients.model.idempotentkey.IdempotentKeys;
+import uk.gov.hmcts.reform.waworkflowapi.clients.service.IdempotentKeysRepository;
 import uk.gov.hmcts.reform.waworkflowapi.utils.AuthorizationHeadersProvider;
 
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import static net.serenitybdd.rest.SerenityRest.given;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -24,6 +29,9 @@ public class SendMessageTest extends SpringBootFunctionalBaseTest {
 
     @Autowired
     private AuthorizationHeadersProvider authorizationHeadersProvider;
+
+    @Autowired
+    private IdempotentKeysRepository idempotentKeysRepository;
 
     private String serviceAuthorizationToken;
     private String caseId;
@@ -195,6 +203,70 @@ public class SendMessageTest extends SpringBootFunctionalBaseTest {
             .post()
             .then()
             .statusCode(HttpStatus.INTERNAL_SERVER_ERROR_500);
+    }
+
+    @Test
+    public void transition_creates_a_task_and_goes_through_external_task() {
+        String dueDate = ZonedDateTime.now().plusDays(2).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        String idempotentKey =  UUID.randomUUID().toString();
+        Map<String, DmnValue<?>> processVariables = mockProcessVariables(
+            dueDate,
+            "Provide Respondent Evidence",
+            "provideRespondentEvidence",
+            "external"
+        );
+
+        processVariables.put("idempotentKey", DmnValue.dmnStringValue(idempotentKey));
+
+        given()
+            .relaxedHTTPSValidation()
+            .header(SERVICE_AUTHORIZATION, serviceAuthorizationToken)
+            .contentType(APPLICATION_JSON_VALUE)
+            .body(new SendMessageRequest(
+                "createTaskMessage",
+                processVariables,
+                null
+            )).log().body()
+            .baseUri(testUrl)
+            .basePath("/workflow/message")
+            .when()
+            .post()
+            .then()
+            .statusCode(HttpStatus.NO_CONTENT_204);
+
+        waitSeconds(2);
+
+        String taskId = given()
+            .header(SERVICE_AUTHORIZATION, serviceAuthorizationToken)
+            .contentType(APPLICATION_JSON_VALUE)
+            .baseUri(camundaUrl)
+            .basePath("/task")
+            .param("processVariables", "caseId_eq_" + caseId)
+            .when()
+            .get()
+            .prettyPeek()
+            .then()
+            .body("size()", is(1))
+            .body("[0].name", is("Provide Respondent Evidence"))
+            .body("[0].formKey", is("provideRespondentEvidence"))
+            .extract()
+            .path("[0].id");
+
+        given()
+            .header(SERVICE_AUTHORIZATION, serviceAuthorizationToken)
+            .contentType(APPLICATION_JSON_VALUE)
+            .baseUri(camundaUrl)
+            .basePath("/task/" + taskId + "/identity-links?type=candidate")
+            .when()
+            .get()
+            .prettyPeek()
+            .then()
+            .body("[0].groupId", is("external"));
+
+        Optional<IdempotentKeys> savedEntity = idempotentKeysRepository.findById(new IdempotentId(idempotentKey,"ia"));
+        assertThat(savedEntity.isPresent()).isTrue();
+
+        cleanUp(taskId, serviceAuthorizationToken);
     }
 
 
