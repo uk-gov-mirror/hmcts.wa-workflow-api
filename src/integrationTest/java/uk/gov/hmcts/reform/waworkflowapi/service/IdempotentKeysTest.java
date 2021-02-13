8aac6545187f3e45b76b4c7cbf3020826d8d4202
david.crespo <david.crespo@hmcts.net>
2021-02-13 03:15:03 +0000

    add jpaRepo because it seems more consistency regarding locks

--- src/integrationTest/java/uk/gov/hmcts/reform/waworkflowapi/service/IdempotentKeysTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/waworkflowapi/service/IdempotentKeysTest.java
@@ -2,30 +2,34 @@ package uk.gov.hmcts.reform.waworkflowapi.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.awaitility.Awaitility;
-import org.junit.Ignore;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.dao.PessimisticLockingFailureException;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.waworkflowapi.clients.model.idempotentkey.IdempotentId;
 import uk.gov.hmcts.reform.waworkflowapi.clients.model.idempotentkey.IdempotentKeys;
 import uk.gov.hmcts.reform.waworkflowapi.clients.service.IdempotentKeysRepository;
+import uk.gov.hmcts.reform.waworkflowapi.clients.service.JpaRepo;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
+import javax.persistence.LockTimeoutException;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @Slf4j
+@ActiveProfiles("integration")
 class IdempotentKeysTest {
 
     @Autowired
     private IdempotentKeysRepository repository;
+    @Autowired
+    private JpaRepo jpaRepo;
     private IdempotentKeys idempotentKeysWithRandomId;
     private IdempotentId randomIdempotentId;
 
@@ -45,7 +49,6 @@ class IdempotentKeysTest {
     }
 
     @Test
-    @Ignore(value = "this test is supposed to run on the minikube local env to check the lock mechanism in postgres db")
     void given_readQueryOnRow_then_anotherQueryOnSameRowThrowException() {
         repository.save(idempotentKeysWithRandomId);
 
@@ -53,18 +56,10 @@ class IdempotentKeysTest {
             log.info("start query1-thread...");
 
             // set exclusive lock on row
-            repository.findById(randomIdempotentId);
+            jpaRepo.findById(randomIdempotentId);
 
-            // simulates isDuplicate processing time
+            // simulates isDuplicate checking takes some time
             Awaitility.await().timeout(15, TimeUnit.SECONDS);
-
-            // finally update the row
-            repository.save(new IdempotentKeys(
-                randomIdempotentId,
-                "updated",
-                LocalDateTime.now(),
-                LocalDateTime.now()
-            ));
 
         }, "query1-thread");
 
@@ -73,10 +68,16 @@ class IdempotentKeysTest {
         Awaitility.await().timeout(2, TimeUnit.SECONDS);
 
         log.info("start query2...");
-        Assertions.assertThrows(
-            PessimisticLockingFailureException.class,
-            () -> repository.findById(randomIdempotentId)
-        );
+
+        try {
+            jpaRepo.findById(randomIdempotentId);
+        } catch (LockTimeoutException e) {
+            Assertions.assertTrue(true);
+            return;
+        }
+
+        Assertions.fail();
+
     }
 
 }
