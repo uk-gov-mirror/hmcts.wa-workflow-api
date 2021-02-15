3137ffa35d0f953404a0228965bcc8bb0f335f6f
david.crespo <david.crespo@hmcts.net>
2021-02-15 09:38:58 +0000

    remove unused import

--- src/integrationTest/java/uk/gov/hmcts/reform/waworkflowapi/service/IdempotentKeysTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/waworkflowapi/service/IdempotentKeysTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.waworkflowapi.clients.model.idempotentkey.IdempotentId;
 import uk.gov.hmcts.reform.waworkflowapi.clients.model.idempotentkey.IdempotentKeys;
