6459d110fd928a414defb83bba6b2047c2fbef8c
David Crespo <david.crespo@hmcts.net>
2021-02-18 17:51:25 +0000

    add worker to process task (#156)

---
+++ src/main/java/uk/gov/hmcts/reform/waworkflowapi/clients/service/IdempotencyTaskWorker.java
@@ -0,0 +1,52 @@
+package uk.gov.hmcts.reform.waworkflowapi.clients.service;
+
+import org.camunda.bpm.client.ExternalTaskClient;
+import org.camunda.bpm.client.task.ExternalTask;
+import org.camunda.bpm.client.task.ExternalTaskService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.waworkflowapi.config.ServiceAuthProviderInterceptor;
+
+import java.util.Map;
+
+import static java.util.Collections.singletonMap;
+
+@Component
+public class IdempotencyTaskWorker {
+    private final String camundaUrl;
+
+    private final AuthTokenGenerator authTokenGenerator;
+
+    public IdempotencyTaskWorker(
+        @Value("${camunda.url}") String camundaUrl,
+        AuthTokenGenerator authTokenGenerator
+    ) {
+        this.camundaUrl = camundaUrl;
+        this.authTokenGenerator = authTokenGenerator;
+    }
+
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void setupClient() {
+        ExternalTaskClient client = ExternalTaskClient.create()
+            .baseUrl(camundaUrl)
+            .addInterceptor(new ServiceAuthProviderInterceptor(authTokenGenerator))
+            .build();
+
+        client.subscribe("idempotencyCheck")
+            .lockDuration(1000)
+            .handler(this::checkIdempotency)
+            .open();
+    }
+
+    public void checkIdempotency(ExternalTask externalTask, ExternalTaskService externalTaskService) {
+        Map<String, Object> processVariables = singletonMap(
+            "isDuplicate",
+            false
+        );
+        externalTaskService.complete(externalTask, processVariables);
+    }
+}
