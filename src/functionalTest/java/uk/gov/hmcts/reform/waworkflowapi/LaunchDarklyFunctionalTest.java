006aeebffda9b685063369cd7d92001c6ee3bad5
lewis150193 <lewis.williams@hmcts.net>
2021-03-04 16:28:11 +0000

    changed client name and added constructors

--- src/functionalTest/java/uk/gov/hmcts/reform/waworkflowapi/LaunchDarklyFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/waworkflowapi/LaunchDarklyFunctionalTest.java
@@ -2,20 +2,23 @@ package uk.gov.hmcts.reform.waworkflowapi;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import uk.gov.hmcts.reform.waworkflowapi.common.LaunchDarklyFunctionalTestClient;
+import uk.gov.hmcts.reform.waworkflowapi.common.LaunchDarklyClient;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class LaunchDarklyFunctionalTest extends SpringBootFunctionalBaseTest {
 
-    @Autowired
-    private LaunchDarklyFunctionalTestClient launchDarklyFunctionalTestClient;
+    private final LaunchDarklyClient launchDarklyClient;
 
+    @Autowired
+    public LaunchDarklyFunctionalTest(LaunchDarklyClient launchDarklyClient) {
+        this.launchDarklyClient = launchDarklyClient;
+    }
 
     @Test
     public void should_hit_launch_darkly() {
-        boolean launchDarklyFeature = launchDarklyFunctionalTestClient.getKey("tester");
+        boolean launchDarklyFeature = launchDarklyClient.getKey("tester");
 
         assertThat(launchDarklyFeature, is(true));
     }
