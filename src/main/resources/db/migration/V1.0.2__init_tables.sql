2896923f9a594f1a6d7b5b00ed776164e5dc49c3
Alejandro Baeza <42570659+alexbaeza@users.noreply.github.com>
2021-03-05 15:35:44 +0000

    Rever migration (#175)

--- src/main/resources/db/migration/V1.0.2__init_tables.sql
+++ src/main/resources/db/migration/V1.0.2__init_tables.sql
@@ -8,9 +8,8 @@ CREATE TABLE idempotent_keys
     idempotency_key varchar(200) NOT NULL,
     tenant_id       varchar(20)  NOT NULL,
     process_id      varchar(200) NOT NULL,
-    created_at      TIMESTAMP    NOT NULL default CURRENT_TIMESTAMP,
-    last_updated_at TIMESTAMP    NOT NULL default CURRENT_TIMESTAMP,
-    PRIMARY KEY (idempotency_key, tenant_id)
-);
+    created_at TIMESTAMP NOT NULL default CURRENT_TIMESTAMP,
+    last_updated_at TIMESTAMP NOT NULL default CURRENT_TIMESTAMP,
+    PRIMARY KEY (idempotency_key, tenant_id));
 
 COMMIT;
