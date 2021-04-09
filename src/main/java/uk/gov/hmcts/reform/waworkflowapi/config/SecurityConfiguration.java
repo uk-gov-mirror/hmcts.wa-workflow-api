2ce2f569abdcece3d5cc7156f8e8d626926c24d5
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-04-09 10:00:04 +0100

    Clean up security configuration

--- src/main/java/uk/gov/hmcts/reform/waworkflowapi/config/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/waworkflowapi/config/SecurityConfiguration.java
@@ -1,15 +1,23 @@
 package uk.gov.hmcts.reform.waworkflowapi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import uk.gov.hmcts.reform.auth.checker.core.RequestAuthorizer;
-import uk.gov.hmcts.reform.auth.checker.core.service.Service;
-import uk.gov.hmcts.reform.auth.checker.spring.serviceonly.AuthCheckerServiceOnlyFilter;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.JwtTimestampValidator;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+import uk.gov.hmcts.reform.authorisation.filters.ServiceAuthFilter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,16 +30,12 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final List<String> anonymousPaths = new ArrayList<>();
-    private final RequestAuthorizer<Service> serviceRequestAuthorizer;
-    private final AuthenticationManager authenticationManager;
+    private final ServiceAuthFilter serviceAuthFiler;
 
-    public SecurityConfiguration(
-        RequestAuthorizer<Service> serviceRequestAuthorizer,
-        AuthenticationManager authenticationManager
-    ) {
+    @Autowired
+    public SecurityConfiguration(final ServiceAuthFilter serviceAuthFiler) {
         super();
-        this.serviceRequestAuthorizer = serviceRequestAuthorizer;
-        this.authenticationManager = authenticationManager;
+        this.serviceAuthFiler = serviceAuthFiler;
     }
 
     public List<String> getAnonymousPaths() {
@@ -50,13 +54,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        AuthCheckerServiceOnlyFilter authCheckerServiceOnlyFilter = new AuthCheckerServiceOnlyFilter(
-            serviceRequestAuthorizer);
-
-        authCheckerServiceOnlyFilter.setAuthenticationManager(authenticationManager);
-
         http
-            .addFilter(authCheckerServiceOnlyFilter)
+            .addFilterBefore(serviceAuthFiler, AbstractPreAuthenticatedProcessingFilter.class)
             .sessionManagement().sessionCreationPolicy(STATELESS)
             .and()
             .csrf().disable()
@@ -64,5 +63,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .logout().disable()
             .authorizeRequests().anyRequest().authenticated()
         ;
+    }
+
+    @Bean
+    @Primary
+    JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(issuerUri);
+
+        OAuth2TokenValidator<Jwt> withTimestamp = new JwtTimestampValidator();
+        OAuth2TokenValidator<Jwt> validator = new DelegatingOAuth2TokenValidator<>(withTimestamp);
+
+        jwtDecoder.setJwtValidator(validator);
+
+        return jwtDecoder;
     }
 }
