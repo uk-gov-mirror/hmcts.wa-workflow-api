883880f2bdb9aa5ad88837a1597f018e40a7e436
raghera <raghera@hotmail.com>
2021-05-25 11:22:59 +0100

    Updating swagger docs. (#232)

--- src/main/java/uk/gov/hmcts/reform/waworkflowapi/controllers/startworkflow/CreateTaskController.java
+++ src/main/java/uk/gov/hmcts/reform/waworkflowapi/controllers/startworkflow/CreateTaskController.java
@@ -31,6 +31,13 @@ import static org.springframework.http.ResponseEntity.noContent;
 )
 public class CreateTaskController {
 
+    public static final String UNAUTHORIZED = "Unauthorized";
+    private static final String BAD_REQUEST = "Bad Request";
+    private static final String FORBIDDEN = "Forbidden";
+    private static final String UNSUPPORTED_MEDIA_TYPE = "Unsupported Media Type";
+    private static final String INTERNAL_SERVER_ERROR = "Internal Server Error";
+    private static final String NOT_FOUND = "Not Found";
+
     private final EvaluateDmnService evaluateDmnService;
     private final SendMessageService sendMessageService;
 
@@ -42,11 +49,36 @@ public class CreateTaskController {
         this.sendMessageService = sendMessageService;
     }
 
-    @PostMapping(path = "/workflow/decision-definition/key/{key}/tenant-id/{tenant-id}/evaluate")
-    @ApiOperation("Creates a message form camunda")
+    @PostMapping(
+        path = "/workflow/decision-definition/key/{key}/tenant-id/{tenant-id}/evaluate"
+    )
+    @ApiOperation("Evaluates business rules given the specified decision definition key.")
     @ApiResponses({
-        @ApiResponse(code = 200, message = "A DMN was found, evaluated and returned",
-            response = EvaluateDmnResponse.class)
+        @ApiResponse(
+            code = 200,
+            message = "A decision definition was found, evaluated and it's output returned.",
+            response = EvaluateDmnResponse.class
+        ),
+        @ApiResponse(
+            code = 403,
+            message = FORBIDDEN
+        ),
+        @ApiResponse(
+            code = 401,
+            message = UNAUTHORIZED
+        ),
+        @ApiResponse(
+            code = 404,
+            message = NOT_FOUND
+        ),
+        @ApiResponse(
+            code = 415,
+            message = UNSUPPORTED_MEDIA_TYPE
+        ),
+        @ApiResponse(
+            code = 500,
+            message = INTERNAL_SERVER_ERROR
+        )
     })
     public ResponseEntity<EvaluateDmnResponse> evaluateDmn(@RequestBody EvaluateDmnRequest evaluateDmnRequest,
                                                            @PathVariable(name = "key") String key,
@@ -62,9 +94,33 @@ public class CreateTaskController {
     }
 
     @PostMapping(path = "/workflow/message")
-    @ApiOperation("Creates a message form camunda")
+    @ApiOperation("Sends a message to the underlying business process engine.")
     @ApiResponses({
-        @ApiResponse(code = 204, message = "A new message was initiated", response = Object.class)
+        @ApiResponse(
+            code = 204,
+            message = "The message was correlated to a business process",
+            response = Object.class
+        ),
+        @ApiResponse(
+            code = 400,
+            message = BAD_REQUEST
+        ),
+        @ApiResponse(
+            code = 403,
+            message = FORBIDDEN
+        ),
+        @ApiResponse(
+            code = 401,
+            message = UNAUTHORIZED
+        ),
+        @ApiResponse(
+            code = 415,
+            message = UNSUPPORTED_MEDIA_TYPE
+        ),
+        @ApiResponse(
+            code = 500,
+            message = INTERNAL_SERVER_ERROR
+        )
     })
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity<Void> sendMessage(@RequestBody SendMessageRequest sendMessageRequest) {
