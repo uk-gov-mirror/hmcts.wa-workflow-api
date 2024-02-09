bbade9f111af487132276ac693ca1a89dacdaff2
raghera <raghera@hotmail.com>
2024-02-09 14:18:48 +0000

    Upgrading to latest version and fix backward compatibility issues (#648)

--- src/main/java/uk/gov/hmcts/reform/waworkflowapi/Application.java
+++ src/main/java/uk/gov/hmcts/reform/waworkflowapi/Application.java
@@ -6,11 +6,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
-@EnableFeignClients(basePackages =
-    {
-        "uk.gov.hmcts.reform.authorisation",
-        "uk.gov.hmcts.reform.waworkflowapi",
-    })
+@EnableFeignClients(basePackages = {
+    "uk.gov.hmcts.reform.authorisation",
+    "uk.gov.hmcts.reform.waworkflowapi",
+})
 public class Application {
 
     public static void main(final String[] args) {
